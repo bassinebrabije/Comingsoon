e03e85cb7abfc8dcba5c2d87e78b9832f4bf5593
bassinebrabije <brabije.yassine123@gmail.com>
2025-07-18 17:49:56 +0100

    date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,6 @@ export default function ComingSoon() {
         setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
         return;
       }
-
       const days = Math.floor(difference / (1000 * 60 * 60 * 24));
       const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
